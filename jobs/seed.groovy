de7bc7e4e27a2e0417372195eab0e36ec3428c72
zdeveloper <zdveloper@gmail.com>
2017-08-08 11:06:10 -0500

    cleaned up code and added slack integration
    Change-Id: Ic8c12f298a9b45ef0c8629ce3f3c9e748b64489e

--- jobs/seed.groovy
+++ jobs/seed.groovy
@@ -1,21 +1,50 @@
 // If you want, you can define your seed job in the DSL and create it via the REST API.
 // See https://github.com/sheehan/job-dsl-gradle-example#rest-api-runner
 
-job('seed') {
+def branch = GIT_BRANCH
+def project = GIT_REPO + "-" + GIT_PROJECT + "-" + branch
+
+def job_name = project +'/' + 'seed'
+
+folder(project){
+    displayName(project)
+}
+
+listView(project) {
+    description('OpenStack-Helm CI')
+    jobs {
+        name(project)
+    }
+    columns {
+        status()
+        weather()
+        name()
+        lastSuccess()
+        lastFailure()
+        lastDuration()
+        buildButton()
+    }
+}
+
+job(job_name) {
+    parameters {
+        stringParam('GIT_PROJECT', GIT_PROJECT)
+        stringParam('GIT_REPO', GIT_REPO)
+        stringParam('GIT_BRANCH', branch)
+        stringParam('SLACK_TEAM', SLACK_TEAM)
+        stringParam('SLACK_TOKEN', SLACK_TOKEN)
+        stringParam('SLACK_ROOM', SLACK_ROOM)
+    }
     scm {
-        github('att-comdev/jenkins-ci', 'att-comdev/jenkins-ci')
+        github("\$GIT_REPO/\$GIT_PROJECT", "\$GIT_BRANCH")
     }
     triggers {
-        scm 'H/5 * * * *'
+        githubPush()
     }
     steps {
-        gradle 'clean test'
         dsl {
-            external 'jobs/**/*Jobs.groovy'
+            external 'jobs/*_job.groovy'
             additionalClasspath 'src/main/groovy'
         }
-    }
-    publishers {
-        archiveJunit 'build/test-results/**/*.xml'
     }
 }
